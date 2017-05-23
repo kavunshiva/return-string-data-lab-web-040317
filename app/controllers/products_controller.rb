36169ca70db348e608d591809377eb604badc1ef
kavunshiva <kavunshiva@gmail.com>
2017-05-23 16:17:27 -0400

    i do it i doooo iiiiit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,37 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def new
+    @product = Product.new
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory.present? && product.inventory > 0 ? "true" : "false"
+  end
+
+  def create
+    @product = Product.new(product_params(
+      :name,
+      :price,
+      :description,
+      :inventory
+    ))
+    if @product.save()
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def product_params(*args)
+    params.require(:product).permit(*args)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get 'description/:id', to: 'products#description'
+  get 'inventory/:id', to: 'products#inventory'
+  resources :products
   resources :orders
   resources :invoices
 end
